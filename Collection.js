78e04c1798f365bb86fc8b305f85ac8e9038273f
张昊 <dzxx0563zh@126.com>
2015-01-13 12:52:24 +0800

    extend

--- Collection.js
+++ Collection.js
@@ -1,22 +1,35 @@
-;(function (){
-	function Collection (){
-		var all = [];
-		function factory () {
+;(function (window){
+	// 常用函数
+	var isObject = function (obj) {
+		var type = typeof obj;
+		return type === "object" && !!obj;
+	};
+	
+	// $ is jQuery
+	var $ = window.jQuery || window.$;
+	
+	// init 初始化函数
+	var init = function (obj){		
+		var all = []; // 私有变量，只能通过方法来访问。
+		
+		// Collection的主体
+		var factory = function () {
 			//  P606
 	 		/* Object.seal(this);  */
- 		}
+ 		};
 	
 		factory.prototype = {
 			constructor: factory,
-			extend: function (name ,func) {
-				if (name === "" || typeof func !== "function") return ;
+			extend: function (obj) {
+				if (!isObject(obj)) return ;
 				
-				// 检测函数是否重名
-				if (name in this) {
-					console.log("函数名重复了");
-					return ;
+				var key;
+				for (key in obj) {
+					if (obj.hasOwnProperty(key) && !(key in this)) {
+						this[key] = obj[key];
+					}
 				}
-				this[name] = func;
+				
 				return this;
 			},
 			push: function (o) {
@@ -29,30 +42,31 @@
 			getAtIndex: function (index) {
 				// 检测是否为数字
 				if (typeof index !== "number") {
-					console.log("function getAtIndex's param must be a number !");
+					console.log("参数必需是数字");
 					return ;
 				} 
 				
 				// 是否越界
 				var tempLength = all.length;
 				if (index > -- tempLength) {
-					console.log("factory range index !");
+					console.log("数组越界");
 					return ;
 				}
 				
-				return all[index] ? all[index] : void 0;
+				var cur = all[index]
+				return cur ? cur : void 0;
 			},
 			deleteAtIndex: function (index) {
 				// 检测是否为数字
 				if (typeof index !== "number") {
-					console.log("function getAtIndex's param must be a number !");
+					console.log("参数必需是数字");
 					return ;
 				}
 				
 				// 是否越界
 				var tempLength = all.length;
 				if (index > -- tempLength) {
-					console.log("factory range index !");
+					console.log("数组越界");
 					return ;
 				}
 				
@@ -67,16 +81,36 @@
 				all.length = 0;
 				return this;
 			},
+			all: function () {
+				return all.slice();	
+			},
 			first: function () {
-				return all[0] ? all[0] : void 0;
+				var first = all[0];
+				return first ? first : void 0;
 			},
 			last: function () {
-				return all[all.length - 1] ? all[all.length - 1] : void 0;
+				var last = all[all.length - 1]
+				return last ? last : void 0;
 			}
-		} 
+		};
+		
+		// 可以用jQ对象数组来初始化Collection
+		if (!!obj && obj instanceof $) {
+			var coll = new factory();
+			$(obj).each(function (index, ele) {
+				coll.push(ele);
+			});
+			
+			return coll;
+		}
 		
 		return new factory ();
 	}
 	
+	// 只暴露出Collection，保护init
+	var Collection  = function (obj) {
+		return new init(obj);
+	};
+
 	window.Collection = Collection;
-}());
+}(window));
