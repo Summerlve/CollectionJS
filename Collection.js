812209161f37db1aea74812d2ba8fad2a440dd3e
张昊 <dzxx0563zh@126.com>
2015-04-02 08:31:20 +0800

    0.02

--- Collection.js
+++ Collection.js
@@ -1,9 +1,6 @@
 //Collection
 ;(function (window){
-	var version = 0.01;
-	
-	// $ is jQuery
-	var $ = window.$ || window.jQuery ;
+	var version = 0.02;
 
 	// 通用函数
 	var isObject = function (obj) {
@@ -11,7 +8,7 @@
 		return type === "object" && !!obj;
 	};
 	
-	// Element、Collection共用函数
+	// 扩展函数
 	var extend = function (obj) {
 		if (!isObject(obj)) return ;
 		
@@ -26,7 +23,7 @@
 	};
 	
 	// Collection的初始化函数
-	var CollectionInit = function (obj){		
+	var CollectionInit = function (obj) {		
 		var all = []; // 私有变量，只能通过方法来访问。
 		
 		// Collection的主体
@@ -48,14 +45,14 @@
 			},
 			add: function (o) {
 				// add is alias of push
-				all.push(o);
+				this.push(o);
 				return this;
 			},
 			pop: function () {
 				return all.pop();
 			},
 			each: function (fn) {
-				// fn(currentValue, index)
+				// fn (currentValue, index)
 				all.forEach(function (currentValue, index) {
 					fn.apply(currentValue, [currentValue, index]);
 				});
@@ -63,13 +60,13 @@
 			},
 			map: function (fn) {
 				var result = [];
-				// fn(currentValue, index)
+				// fn (currentValue, index)
 				all.forEach(function (currentValue, index) {
 					result.push(fn.apply(currentValue, [currentValue, index]));
 				});
 				return result;
 			},
-			getAtIndex: function (index) {
+			get: function (index) {
 				// 检测是否为数字
 				if (typeof index !== "number") {
 					console.log("参数必需是数字");
@@ -86,7 +83,7 @@
 				var cur = all[index]
 				return cur ? cur : void 0;
 			},
-			deleteAtIndex: function (index) {
+			del: function (index) {
 				// 检测是否为数字
 				if (typeof index !== "number") {
 					console.log("参数必需是数字");
@@ -102,6 +99,11 @@
 				
 				// 删除
 				all.splice(index, 1);
+				return this;
+			},
+			remove: function (index) {
+				// remove is alias of del
+				this.del(index);
 				return this;
 			},
 			length: function () {
@@ -125,46 +127,14 @@
 			}
 		};
 		
-		// 可以用jQ对象数组来初始化Collection
-		if (!!obj && obj instanceof $) {
-			var coll = new factory();
-			$(obj).each(function (index, ele) {
-				coll.push(ele);
-			});
-			
-			return coll;
-		}
-		
 		return new factory ();
 	}
-	
-	// El的初始化函数
-	var ElInit = function () {
-	
-		// Element的主体
-		var factory = function () {
-				
-		};
 		
-		factory.prototype = {
-			version: version,
-			constructor: factory,
-			extend: extend
-		}
-				
-		return new factory ();
-	};
-	
 	// 只暴露出Collection，保护CollectionInit
 	var Collection  = function (obj) {
 		return new CollectionInit(obj);
 	};
 	
-	// 只暴露出El，保护ElInit
-	var El  = function (obj) {
-		return new ElInit(obj);
-	};
-	
+	// 映射成全局变量
 	window.Collection = Collection;
-	window.El = El; 
 }(window));
