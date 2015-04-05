6abbb2d37cb3d03ce839a22098026484617d9d88
张昊 <dzxx0563zh@126.com>
2015-04-05 16:27:10 +0800

    some change

--- Collection.js
+++ Collection.js
@@ -1,5 +1,5 @@
 "use strict"
-//Collection
+//CollectionJS
 ;(function (window){
 	var version = 0.11;
 		
@@ -27,34 +27,7 @@
 		return toString.call(o) === "[object String]"
 	}
 	
-	
-	// 单向链表
-	function SingleLinkedList () {
-		console.log(this);
-		this.head = this;
-		this.tail = this;
-		this.next = null;
-	}
-	
-	SingleLinkedList.prototype = {
-		constructor: SingleLinkedList,
-		addToTail: function (data) {
-			var o = {
-				data: data,
-				next: null
-			};
-			this.tail.next = o;
-			this.tail = o;
-		},
-		display: function () {
-			var cur = this.next;
-			while (cur !== null) {
-				console.log(cur.data);
-				cur = cur.next;
-			}
-		}
-	}
-		
+			
 	// 用于全局搜索数据结构，和tag搭配，Stack与Queue共用
 	function Lib () {
 		this.store =  [];
@@ -95,95 +68,56 @@
 	Collection.prototype = {
 		version: version,
 		constructor: Collection,
-		/*
-		extend: function (obj) {
-			if (!isObject(obj)) return ;
-			
-			var key;
-			for (key in obj) {
-				if (obj.hasOwnProperty(key) && !(key in this)) {
-					this[key] = obj[key];
-				}
-			}
-			
-			return this;
-		},
-		*/
 		tag: "", // 用于全局搜索数据结构，必须是字符串，不能重复。
 		display: function () {
 			// just for test			
 			console.log(this.all);	
 		},
-		// Traverse Method
-		each: function (fn) {
-			// fn (currentValue, index)，this = currentValue
-			this.all.forEach(function (currentValue, index) {
-				// 屏蔽forEach回调函数的第三个参数array
-				fn.call(currentValue, currentValue, index);
-			});
-			return this;
-		},
-		map: function (fn) {
-			var result = [];
-			// fn (currentValue, index)，this = currentValue
-			this.all.forEach(function (currentValue, index) {
-				// 屏蔽forEach回调函数的第三个参数array
-				result.push(fn.call(currentValue, currentValue, index));
-			});
-			return result;
-		},
-		// General Method
-		get: function (index) {
-			// 检测是否为数字
-			if (!isNumber(index)) {
-				throw "get()的参数必需是数字";
-			} 
-			
-			// 是否越界
-			var tempLength = this.all.length;
-			if (index > -- tempLength) {
-				throw "数组越界";
-			}
-			
-			var cur = this.all[index]
-			return cur ? cur : void 0;
-		},
-		remove: function (index) {
-			// 检测是否为数字
-			if (!isNumber(index)) {
-				throw "remove()的参数必需是数字";
-			}
-			
-			// 是否越界
-			var tempLength = this.all.length;
-			if (index > -- tempLength) {
-				throw "数组越界";
-			}
-			
-			// 删除
-			this.all.splice(index, 1);
-			return this;
-		},
-		length: function () {
+		// Using Method
+		size: function () {
 			return this.all.length;
 		},
-		empty: function () {
-			this.all.length = 0;
-			return this;
-		},
-		toArray: function () {
-			// return a Array copy
-			return this.all.slice();	
-		},
-		first: function () {
-			var first = this.all[0];
-			return first ? first : void 0;
-		},
-		last: function () {
-			var last = this.all[this.all.length - 1]
-			return last ? last : void 0;
+		isEmpty: function () {
+			return this.all.length === 0 ? true : false;
 		}
 	};
+	
+	// SingleLinkedList
+	function SingleLinkedList () {
+		this.head = this;
+		this.tail = this;
+		this.next = null;
+		this.length = 0;
+	}
+	
+	SingleLinkedList.prototype = {
+		constructor: SingleLinkedList,
+		addToTail: function (data) {
+			var o = {
+				data: data,
+				next: null
+			};
+			this.tail.next = o;
+			this.tail = o;
+			++ this.length;
+		},
+		display: function () {
+			var cur = this.head.next;
+			var result = ["Head"];
+			while (cur !== null) {
+				result.push(cur.data);
+				cur = cur.next;
+			}
+			console.log(result.join(" -> "));
+		},
+		size: function () {
+			return this.length;	
+		},
+		isEmpty: function () {
+			return this.length === 0 ? true : false;
+		}
+	};
+
 	
 	// Stack 
 	function Stack (tag) {
@@ -199,13 +133,20 @@
 	var StackProto = Stack.prototype;
 	StackProto.constructor = Stack;
 	
-	StackProto.push = function () {
-		push.apply(this.all, arguments);
+	StackProto.push = function (o) {
+		this.all.push(o);		
 		return this;
 	};
 		
 	StackProto.pop = function () {
 		return this.all.pop();
+	};
+	
+	StackProto.foreach = function (fn) {
+		for (var i = this.all.length - 1; i >= 0; i--) {
+			var cvl = this.all[i];
+			fn.call(cvl, cvl);
+		}
 	};
 	
 	
@@ -222,13 +163,21 @@
 	var QueueProto = Queue.prototype;
 	QueueProto.constructor = Queue;
 	
-	QueueProto.unshift = function () {
-		unshift.apply(this.all, arguments);
+	QueueProto.enqueue = function (o) {
+		this.all[this.all.length] = o;
 		return this;	
 	};
 	
-	QueueProto.shift = function () {
+	QueueProto.dequeue = function () {
+		if (this.all.length === 0) throw "队列已空";
 		return this.all.shift();
+	};
+	
+	QueueProto.foreach = function (fn) {
+		for (var i = 0; i < this.all.length; i++) {
+			var cvl = this.all[i];
+			fn.call(cvl, cvl);
+		}
 	};
 	
 	// 映射成全局变量
